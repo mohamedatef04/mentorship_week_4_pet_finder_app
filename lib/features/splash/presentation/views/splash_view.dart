6692550a37f3a26c5a382eb06cc5966357a05dbd
mohamed atef <smsm85579@gmail.com>
2025-10-15 15:53:14 +0300

    - add onboarding_view to routes - navigate from splash to onboarding - add custom container widget - finish on boarding view ui

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
 import 'package:pet_finder_app_task/core/utils/assets.dart';
+import 'package:pet_finder_app_task/features/on_boarding/presentation/views/on_boarding_view.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
   static const routeName = '/';
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 2)).then(
+      (value) => GoRouter.of(context).pushReplacement(OnBoardingView.routeName),
+    );
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: SizedBox(
