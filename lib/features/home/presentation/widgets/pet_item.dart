861275e6ab379e56d7367ec7351e87656b2c940e
mohamed atef <smsm85579@gmail.com>
2025-10-18 15:27:56 +0300

    - add integration test package - create app_test.dart for integration test - edit pet item

--- lib/features/home/presentation/widgets/pet_item.dart
+++ lib/features/home/presentation/widgets/pet_item.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:go_router/go_router.dart';
 import 'package:pet_finder_app_task/core/constants/api_constants.dart';
 import 'package:pet_finder_app_task/core/theme/app_colors.dart';
 import 'package:pet_finder_app_task/core/theme/app_text_styles.dart';
 import 'package:pet_finder_app_task/features/home/domain/entities/pet_entity.dart';
+import 'package:go_router/go_router.dart';
 import 'package:pet_finder_app_task/features/home/presentation/views/pet_details_view.dart';
 
 class PetItem extends StatelessWidget {
@@ -13,38 +13,39 @@ class PetItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // بناء صورة الحيوان مع placeholder
-    Widget petImage;
-    if (petEntity.petImageUrl.isNotEmpty) {
-      petImage = Container(
-        width: 112.w,
-        height: 112.h,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15.r),
-          color: AppColors.lightGreyColor,
-          image: DecorationImage(
-            image: NetworkImage(
-              '${ApiConstants.baseImageUrl}${petEntity.petImageUrl}.jpg',
-            ),
-            fit: BoxFit.fill,
-          ),
-        ),
-      );
-    } else {
-      petImage = Container(
-        width: 112.w,
-        height: 112.h,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15.r),
-          color: AppColors.lightGreyColor,
-        ),
-        child: const Icon(
-          Icons.pets,
-          size: 50,
-          color: Colors.grey,
-        ), // Placeholder
-      );
-    }
+    // بناء صورة الحيوان مع placeholder وآمان من فشل التحميل
+    Widget petImage = Container(
+      width: 112.w,
+      height: 112.h,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15.r),
+        color: AppColors.lightGreyColor,
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15.r),
+        child: petEntity.petImageUrl.isNotEmpty
+            ? Image.network(
+                '${ApiConstants.baseImageUrl}${petEntity.petImageUrl}.jpg',
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) {
+                  return const Center(
+                    child: Icon(
+                      Icons.pets,
+                      size: 50,
+                      color: Colors.grey,
+                    ),
+                  );
+                },
+              )
+            : const Center(
+                child: Icon(
+                  Icons.pets,
+                  size: 50,
+                  color: Colors.grey,
+                ),
+              ),
+      ),
+    );
 
     return GestureDetector(
       onTap: () {
@@ -65,21 +66,23 @@ class PetItem extends StatelessWidget {
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 5.h),
             child: Row(
-              spacing: 10.w,
               children: [
                 Expanded(child: petImage),
+                SizedBox(width: 10.w),
                 Expanded(
                   flex: 2,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(petEntity.petName, style: AppTextStyles.black18),
+                      SizedBox(height: 5.h),
                       Text(
                         petEntity.petOrigin,
                         style: AppTextStyles.darkGrey16.copyWith(
                           fontSize: 14.sp,
                         ),
                       ),
+                      SizedBox(height: 5.h),
                       Text(
                         '${petEntity.petLifeSpan} Life Span',
                         style: AppTextStyles.darkGrey16.copyWith(
